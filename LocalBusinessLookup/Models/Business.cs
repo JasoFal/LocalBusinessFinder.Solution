8c80bb51771bd060b42020ffeb91a45ed03b69fa
Jason Falk <jasonfalk9@gmail.com>
2024-07-02 15:23:23 -0700

    Add business model.

---
+++ LocalBusinessLookup/Models/Business.cs
@@ -0,0 +1,16 @@
+namespace LocalBusiness.Models
+{
+  public class Business
+  {
+    public int BusinessId { get; set; }
+    public DateTime DateAdded { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Description { get; set; }
+
+    public Business()
+    {
+      DateAdded = DateTime.UtcNow;
+    }
+  }
+}
