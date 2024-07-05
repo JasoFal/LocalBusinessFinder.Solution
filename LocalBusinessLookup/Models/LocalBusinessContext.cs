e11a439b64236ffd3dfc744845e2e075ece4527d
Jason Falk <jasonfalk9@gmail.com>
2024-07-05 10:30:21 -0700

    Add seed data.

--- LocalBusinessLookup/Models/LocalBusinessContext.cs
+++ LocalBusinessLookup/Models/LocalBusinessContext.cs
@@ -9,5 +9,15 @@ namespace LocalBusiness.Models
     public LocalBusinessContext(DbContextOptions<LocalBusinessContext> options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Business>()
+        .HasData(
+          new Business { BusinessId = 1, Name = "Jim's Roadhouse", Type = "Restaurant", Description = "BBQ of all forms!"},
+          new Business { BusinessId = 2, Name = "Terry's Tools", Type = "Shop", Description = "We got all kinds of tools!"},
+          new Business { BusinessId = 3, Name = "Parker's Place", Type = "Place", Description = "Don't step foot in the place."}
+      );
+    }
   }
 }
